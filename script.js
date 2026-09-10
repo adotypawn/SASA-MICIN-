8b2b25f248c53db46a8cfd574ec7037acaf0ba0a
adoty <adityamufrih606@gmail.com>
2026-09-10 13:03:48 +0700

    update script.js

--- script.js
+++ script.js
@@ -52,8 +52,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const memoriesSection = document.getElementById('memories-section'); // Slide 3 (Memori)
     
     const btnNext2 = document.getElementById('btn-next-2');
-    const finaleSection = document.getElementById('finale-section');     // Slide 4 (Finale)
+    const finaleSection = document.getElementById('finale-section');
 
+   const btnNext3 = document.getElementById('btn-next-3');
+    const headerHero = document.getElementById('header');
+  
     // A. Saat tombol OPEN LETTER dipencet -> Slide 1 hilang, Slide 2 muncul
     if (btnOpenLetter && letterSection) {
         btnOpenLetter.addEventListener('click', function(e) {
